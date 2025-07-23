78b66615a80350f95e6c5b739c5ba4e5416e1176
Srishti Aggarwal <aggarwalsrishti881@gmail.com>
2025-07-23 14:36:08 +0530

    Remove .env file from repository

---
+++ StayNestWebsite/frontend/src/utils/constant.js
@@ -0,0 +1,6 @@
+export const USER_API_END_POINT="http://localhost:4000/api/v1/user"
+export const HOTEL_API_END_POINT="http://localhost:4000/api/v1/hotels"
+export const ROOM_API_END_POINT="http://localhost:4000/api/v1/room"
+export const BOOKING_API_END_POINT="http://localhost:4000/api/v1/booking"
+export const STRIPE_PUBLISHABLE_KEY=
+"pk_test_51RmZnk2aQNjgQ1yZGZYA0nRKhHgbQ00LJVNsqc7RNHMo3ZaSQWjRAPfyR7MbMBJ2FJ1RuwMmjwWhAwgtMpZe4Eje004miuYENU"
