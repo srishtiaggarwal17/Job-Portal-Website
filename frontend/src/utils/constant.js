e904165c12cce51ae9bcb3725562919977918364
srishtiaggarwal17 <159170123+srishtiaggarwal17@users.noreply.github.com>
2025-07-30 12:21:40 +0530

    Update constant.js

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -5,7 +5,7 @@
 
 const BASE_URL = import.meta.env.VITE_API_URL;
 
-export const USER_API_END_POINT = `${BASE_URL}/user`;
-export const JOB_API_END_POINT = `${BASE_URL}/job`;
-export const APPLICATION_API_END_POINT = `${BASE_URL}/application`;
-export const COMPANY_API_END_POINT = `${BASE_URL}/company`;
+export const USER_API_END_POINT = `${BASE_URL}/api/v1/user`;
+export const JOB_API_END_POINT = `${BASE_URL}/api/v1/job`;
+export const APPLICATION_API_END_POINT = `${BASE_URL}/api/v1application`;
+export const COMPANY_API_END_POINT = `${BASE_URL}/api/v1/company`;
