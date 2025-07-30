367826b1dfc908cc769f1344cfe6eb11b841cd58
srishtiaggarwal17 <159170123+srishtiaggarwal17@users.noreply.github.com>
2025-07-30 12:53:36 +0530

    Update job.route.js

--- backend/routes/job.route.js
+++ backend/routes/job.route.js
@@ -5,7 +5,7 @@ import { postJob,getAdminJobs, getAllJobs, getJobById, updateJob } from "../cont
 const router=express.Router();
 
 router.route("/post").post(isAuthenticated,postJob)
-router.route("/get").get(isAuthenticated,getAllJobs)
+router.route("/get").get(getAllJobs)
 router.route("/get/:id").get(isAuthenticated,getJobById)
 router.route("/getadminjobs").get(isAuthenticated,getAdminJobs)
 router.route('/update/:id').put(isAuthenticated,updateJob)
