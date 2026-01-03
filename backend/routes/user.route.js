2e2e445564023b61b7beda3e55284a0566deb033
srishtiaggarwal17 <159170123+srishtiaggarwal17@users.noreply.github.com>
2026-01-04 00:50:33 +0530

    Update user.route.js

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from "express"
-import {login,register,updateProfile,logout} from "../controllers/user.controller.js"
+import {login,register,updateProfile,logout,verifyOtp,resendOtp} from "../controllers/user.controller.js"
 import isAuthenticated from "../middlewares/isAuthenticated.js";
 import { singleUpload } from "../middlewares/multer.js";
 
@@ -9,5 +9,7 @@ router.route("/register").post(singleUpload,register)
 router.route("/login").post(login)
 router.route("/profile/update").post(isAuthenticated,singleUpload,updateProfile)
 router.route("/logout").get(logout)
+router.route("/verifyOtp").post(verifyOtp);
+router.route("/resendOtp").post(resendOtp);
 
 export default router;
